296c089c1cc34a07b3a2d1435568955a14293737
Gusarich <danielsedovzzz@gmail.com>
2023-04-28 20:32:23 +0300

    add example deployment script

--- scripts/deployToken.ts
+++ scripts/deployToken.ts
@@ -1,13 +1,21 @@
-import { toNano } from 'ton-core';
+import { Address, toNano } from 'ton-core';
 import { Token } from '../wrappers/Token';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const token = provider.open(Token.createFromConfig({}, await compile('Token')));
+    const token = provider.open(
+        Token.createFromConfig(
+            {
+                name: 'MyCoolToken',
+                symbol: 'MCT',
+                decimals: 9,
+                owner: Address.parse('EQBIhPuWmjT7fP-VomuTWseE8JNWv2q7QYfsVQ1IZwnMk8wL'),
+            },
+            await compile('Token')
+        )
+    );
 
     await token.sendDeploy(provider.sender(), toNano('0.05'));
 
     await provider.waitForDeploy(token.address);
-
-    // run methods on `token`
 }
